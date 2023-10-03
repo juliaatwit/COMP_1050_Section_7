0ee7709332c4c68828611899a1dd810141752660
juliaatwit <144470900+juliaatwit@users.noreply.github.com>
2023-10-03 10:43:54 -0400

    New Program
    added our java program
    
    dayoftheweek

---
+++ src/comp1050Q5/Comp1050Q5.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package comp1050Q5;
+
+/**
+ * 
+ */
+public class Comp1050Q5 {
+
+	/**
+	 * 
+	 */
+	public Comp1050Q5() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("What is your favorite day of the week?");
+
+	}
+
+}
